66a0e9b29a6d10d80b69ce6ceda022f86f66d9a6
Jason <jasongitmail@gmail.com>
2025-05-14 03:11:47 +0000

    show how to extend Env type for secrets in bindings.ts

--- src/lib/bindings.ts
+++ src/lib/bindings.ts
@@ -1,13 +1,19 @@
+interface CloudflareEnv extends Env {
+  SOME_SECRET: string;
+  ANOTHER_SECRET: string;
+}
+
 /**
  *  https://developers.cloudflare.com/workers/frameworks/framework-guides/tanstack
- *
+ *  Run `wrangler types` to generate types from bindings in `wrangler.jsonc` and
+ *  to define the `Env` type. Use the above interface to extend it for env secrets.
  */
 export async function getBindings() {
   if (import.meta.env.DEV) {
     const { getPlatformProxy } = await import("wrangler");
     const { env } = await getPlatformProxy();
-    return env as unknown as Env;
+    return env as unknown as CloudflareEnv;
   }
 
-  return process.env as unknown as Env;
+  return process.env as unknown as CloudflareEnv;
 }
